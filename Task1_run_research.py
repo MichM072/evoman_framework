e035a99d53910a754f08c32b63a729bf457869f3
MichM072 <Michaelsmoor@outlook.com>
2024-09-26 16:55:23 +0200

    Merge with GEO

--- Task1_run_research.py
+++ Task1_run_research.py
@@ -5,8 +5,22 @@
 ###############################################################################
 
 from GA_Spec_SA import GASpecialistSA
+import matplotlib
 
-# Test implementation
+'''
+Steps:
+1. Tune vs enemy 4
+2. Train 10 times vs 3 enemies
+3. Per enemy 10 line max, 10 lines mean
+4. Take average of all max and mean values per enemy.
+5. Take std of max and mean values per enemy.
+6. Repeat for EA2 and combine final plot in final plot
+7. Repeat for all enemies.
+8. Take best individual out of all 10 sims and play against enemy 5 times and measure the gain.
+Gain = Player Lifepoints - Enemy Lifepoints
+9. Generate barplot
+10. Repeat for all enemies.
+'''
 
 enemies = [4, 5, 7]
 
@@ -15,3 +29,6 @@ Normal_agent = GASpecialistSA(SA=False)
 
 for e in enemies:
     Normal_agent.run_experiment(e)
+
+def train_agent():
+    pass
